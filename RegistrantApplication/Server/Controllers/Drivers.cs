13b12e042de085a6556d70d01520a4fc0bd7ed64
Кулагин Алексей <tcrazywolf63@gmail.com>
2023-10-15 18:33:59 +0400

    я

---
+++ RegistrantApplication/Server/Controllers/Drivers.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrantApplication.Server.Database;
+using RegistrantApplication.Shared.API;
+using RegistrantApplication.Shared.Contragents;
+using RegistrantApplication.Shared.Drivers;
+
+namespace RegistrantApplication.Server.Controllers
+{
+    public class Drivers : BaseController
+    {
+        public Drivers(ILogger<BaseController> logger, LiteContext ef) : base(logger, ef)
+        {
+        }
+
+        [HttpGet("Get")]
+        public IActionResult Get(string? search, long page, bool showDeleted)
+        {
+            if (page < 0)
+                return BadRequest("Страница отрицательная");
+
+            const long recordsByPage = 10;
+
+            long totalRecords = _ef.Contragents
+                .Where(x => x.IsDeleted == showDeleted).Count();
+
+            long totalPages = totalRecords / recordsByPage;
+
+            if (page > totalPages)
+                return BadRequest("Страница за пределами количество страниц)");
+
+            List<Driver> data;
+
+            if (string.IsNullOrEmpty(search))
+            {
+                data = _ef.Drivers
+                     .OrderBy(x => x.IdDriver)
+                     .Where(x => x.IsDeleted == showDeleted)
+                     .Skip((int)(page * recordsByPage))
+                     .Take((int)recordsByPage)
+                     .ToList();
+                totalRecords = _ef.Contragents.Where(x => x.IsDeleted == showDeleted).Count();
+            }
+            else
+            {
+                data = _ef.Drivers
+                    .OrderBy(x => x.IdDriver)
+                    .Where(x => (x.IsDeleted == showDeleted) && x.Family.ToUpper().Contains(search.ToUpper()))
+                    .Skip((int)(page * recordsByPage))
+                    .Take((int)recordsByPage)
+                    .ToList();
+
+                totalRecords = _ef.Contragents.Where(x => (x.IsDeleted == showDeleted) && x.Title.ToUpper().Contains(search.ToUpper())).Count();
+                totalPages = totalRecords / recordsByPage;
+            }
+
+            ViewDrivers view = new ViewDrivers()
+            {
+                TotalRecords = totalRecords,
+                TotalPages = totalPages,
+                CurrentPage = page,
+                Drivers = data,
+                MaxRecordsOnPageConst = recordsByPage
+            };
+
+            return Ok(view);
+        }
+
+    }
+}
---
+++ RegistrantApplication/Shared/API/ViewDrivers.cs
@@ -0,0 +1,14 @@
+﻿using RegistrantApplication.Shared.Contragents;
+using RegistrantApplication.Shared.Drivers;
+
+namespace RegistrantApplication.Shared.API
+{
+    public class ViewDrivers
+    {
+        public long CurrentPage { get; set; }
+        public long TotalPages { get; set; }
+        public long TotalRecords { get; set; }
+        public long MaxRecordsOnPageConst { get; set; }
+        public List<Driver> Drivers { get; set; }
+    }
+}

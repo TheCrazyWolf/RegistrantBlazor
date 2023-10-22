d038d1c65006fbcb4b30f60b27b3cd4f7b48fac4
kulagin <alex_kulagin@samgk.ru>
2023-10-22 11:08:34 +0400

    Немного переписал код, но что то отъебнуло

---
+++ RegistrantApplication/Shared/IModal.cs
@@ -0,0 +1,57 @@
+﻿namespace RegistrantApplication.Shared;
+
+public interface IModal
+{
+    
+    /// <summary>
+    /// ID модального окна
+    /// </summary>
+    string IdModal { get; set; }
+    /// <summary>
+    /// Показано ли окно
+    /// </summary>
+    bool IsDisplay { get; set; }
+    
+    /// <summary>
+    /// Заголовок модального окна
+    /// </summary>
+    string PropertyTitle { get; set; }
+    /// <summary>
+    /// Атрибуты заднего фона (анимация, появления TailWind CSS
+    /// </summary>
+    string PropertySmoothShowBackground { get; set; }
+    /// <summary>
+    /// Атрибуты модального окна (анимация, появления TailWind CSS
+    /// </summary>
+    string PropertySmoothShowModal{ get; set; }
+    /// <summary>
+    /// Атрибуты для размытия заднего фона (анимация, появления TailWind CSS
+    /// </summary>
+    string PropertyBlur { get; set; }
+    /// <summary>
+    /// Сообщение об ошибке
+    /// </summary>
+    string PropertyErrorMsg { get; set; }
+    
+    /// <summary>
+    /// Отправка формы
+    /// </summary>
+    void SubmitAsync();
+    /// <summary>
+    /// Показать форму формы
+    /// </summary>
+    Task<bool> ShowAsync();
+    /// <summary>
+    /// Ожидание результата диалога
+    /// </summary>
+    /// <returns></returns>
+    Task<bool> WaitForResult();
+    /// <summary>
+    /// Закрыть
+    /// </summary>
+    void CloseAsync();
+
+
+
+    
+}
